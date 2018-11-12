c78f5fbe1c8ec97db30a0258b5b1f3aea9b399bc
cmaliwal <chiragmaliwal1995@gmail.com>
2018-11-12 13:31:17 +0530

    updated clinical trail contract

--- contracts/ClinicalTrial.sol
+++ contracts/ClinicalTrial.sol
@@ -74,23 +74,58 @@ contract ClinicalTrial {
         _subject = subjects[_id];
     }
 
-    function getDataCounterForSubject(uint _subjectId) public view returns (uint _counter) {
-        if ( _subjectId >= subjects.length ) {
-            _counter = 0;
-            return;
+    function getDataCounterForSubject(uint _subjectId) public view returns (uint) {
+        if (_subjectId >= subjects.length) {
+            return 0;
         }
         bytes32 ident = getSubjectIdentById(_subjectId);
-        _counter = data[ident].length;
+        return data[ident].length;
     }
 
-    function getSubjectIdentById(uint _subjectId) public view returns (bytes32 _ident) {
+    function getSubjectIdentById(uint _subjectId) public view returns (bytes32) {
         if ( _subjectId >= subjects.length ) {
-            _ident = "";
+            return "";
+            
+        }
+        return keccak256(abi.encodePacked(subjects[_subjectId]));
+    }
+
+    function getDataPointForSubject(uint _subjectId, uint _dataPointId) public view returns (uint _timestamp, bytes32 _json) {
+        if ( _subjectId >= subjects.length ) {
+            _timestamp = 0;
+            _json = "";
             return;
         }
-        _ident = keccak256(abi.encodePacked(subjects[_subjectId]));
+
+        bytes32 ident = getSubjectIdentById(_subjectId);
+        if (_dataPointId >= data[ident].length) {
+            _timestamp = 0;
+            _json = "";
+            return;
+        }
+
+        _timestamp = data[ident][_dataPointId].timestamp;
+        _json = data[ident][_dataPointId].json;
     }
 
+    function addSubject(bytes32 _subject) public croOnly dateBeforeStart returns (bool _success) {
+        subjects.push(_subject);
+        emit AddSubject(msg.sender,_subject,block.timestamp);
+        return true;
+    }
 
+    function addDataPoint(uint _subjectId, bytes32 _json) public croOnly trialIdOpen returns (bool _success) {
+        if ( _subjectId >= subjects.length ) {
+            revert("invalid Id");
+        }
+
+        bytes32 ident = getSubjectIdentById(_subjectId);
+        DataPoint memory dp;
+        dp.timestamp = uint32(now);
+        dp.json = _json;
+
+        data[ident].push(dp);
+        return true;
+    }  
 
 }
